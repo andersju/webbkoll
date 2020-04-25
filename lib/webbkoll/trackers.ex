a16854b937d31bce9b1e301edda3497962c8a9b0
Anders Jensen-Urstad <anders@unix.se>
2020-04-25 23:10:33 +0200

    Handle the case where a domain belongs to more than one Disconnect tracker category; use Mozilla's version of the Disconnect tracker list

--- lib/webbkoll/trackers.ex
+++ lib/webbkoll/trackers.ex
@@ -3,30 +3,43 @@ defmodule Webbkoll.Trackers do
   @external_resource Application.app_dir(:webbkoll, "priv/services.json")
 
   # We want to be able to match domains against Disconnect's open source list
-  # of trackers (https://github.com/disconnectme/disconnect-tracking-protection),
+  # of trackers (https://github.com/disconnectme/disconnect-tracking-protection
+  # or Mozilla's version https://github.com/mozilla-services/shavar-prod-lists),
   # however their services.json has the actual domains a few levels deep in the
   # structure. The following parses the JSON file and creates a map where each
   # unique host is a key. Additionally, by making this a module attribute, we
   # ensure that this is only done at compile time.
   @hosts (fn ->
-            categories =
-              Application.app_dir(:webbkoll, "priv/services.json")
-              |> File.read!()
-              |> Jason.decode!()
-              |> Map.get("categories")
+            Application.app_dir(:webbkoll, "priv/services.json")
+            |> File.read!()
+            |> Jason.decode!()
+            |> Map.get("categories")
+            |> Enum.reduce(%{}, fn {category, sites}, hosts ->
+              Enum.reduce(sites, hosts, fn site, hosts ->
+                Enum.reduce(site, hosts, fn {name, url}, hosts ->
+                  url
+                  |> Enum.filter(fn {x, _y} -> String.starts_with?(x, ["http", "www."]) end)
+                  |> Enum.into(%{})
+                  |> Map.values()
+                  |> List.first()
+                  |> Enum.reduce(hosts, fn host, hosts ->
+                    if Map.has_key?(hosts, host) do
+                      new_map = %{
+                        hosts[host]
+                        | "category" => [category | hosts[host]["category"]]
+                      }
 
-            for {category, value} <- categories, organization <- value do
-              {name, org_values} = Enum.at(organization, 0)
-
-              for {url, hosts} <- org_values,
-                  String.starts_with?(url, ["http", "www."]) and is_list(hosts) do
-                for host <- hosts do
-                  {host, "#{category} (#{name})"}
-                end
-              end
-            end
-            |> List.flatten()
-            |> Enum.reject(&is_nil/1)
+                      Map.put(hosts, host, new_map)
+                    else
+                      Map.put_new(hosts, host, %{"category" => [category], "name" => name})
+                    end
+                  end)
+                end)
+              end)
+            end)
+            |> Enum.map(fn {k, v} ->
+              {k, "#{Enum.join(v["category"], ", ")} (#{v["name"]})"}
+            end)
             |> Enum.into(%{})
           end).()
 
