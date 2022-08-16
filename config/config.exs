500ef88919f94407917ce1cb6b77b3f9eb45d887
Anders Jensen-Urstad <anders@unix.se>
2022-08-16 21:18:33 +0200

    public_suffix: enable download_data_on_compile

--- config/config.exs
+++ config/config.exs
@@ -46,6 +46,8 @@ config :webbkoll, Webbkoll.Scheduler,
     {"* * * * *", {Webbkoll.CronJobs, :find_and_remove_stuck_records, []}}
   ]
 
+config :public_suffix, download_data_on_compile: true
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
